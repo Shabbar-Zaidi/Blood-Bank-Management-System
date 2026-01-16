1c0c0f4a02bd9ef33c2eff58b73e532d671f753a
Shabbar Zaidi <shabbar5zaidi@gmail.com>
2026-01-16 10:58:36 +0500

    commit code

---
+++ client/src/components/ProtectedRoute.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { isTokenValid, handleAuthError } from '../utils/auth';
+
+const ProtectedRoute = ({ children }) => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!isTokenValid()) {
+      handleAuthError(navigate);
+    }
+  }, [navigate]);
+
+  // If token is invalid, the useEffect will handle redirect
+  // Return children only if token is valid
+  return isTokenValid() ? children : null;
+};
+
+export default ProtectedRoute;
